259fd51e5da56f9a6ea049090a0082e99be8afcf
someshfengde <someshfengde@gmail.com>
2025-08-12 16:35:35 +0530

    base structure push

---
+++ README.md
@@ -0,0 +1,3 @@
+End to end project with deployment.
+
+Created as a part of krish naik course 
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from end-to-end-project!")
+
+
+if __name__ == "__main__":
+    main()
